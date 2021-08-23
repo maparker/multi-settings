33449ff63457d97fd434c60ae0f2f6d2cf9b282a
maparker <mail@mattparker.com>
2021-08-23 13:22:32 -0400

    Update findinfile.js

--- findinfile.js
+++ findinfile.js
@@ -60,3 +60,12 @@
   });
 })();
 document.addEventListener("dragover", e => e.preventDefault());
+(() => {
+  const listener = e => e.stopPropagation();
+  const query = () => document.querySelectorAll('a[target=_blank]').forEach(a => {
+    a.removeEventListener('click', listener);
+    a.addEventListener('click', listener, true);
+  });
+  query();
+  setInterval(query, 400); // wait time between DOM queries, in milliseconds
+})();
